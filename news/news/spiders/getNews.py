3e6955436f1242fe783e9f19bcdf9f3ccc9d784c
2dukes <ruipinto02@hotmail.com>
2021-10-29 11:56:51 +0100

    Scrapy and Selenium tool to fetch Crypto News from CoinMarketCap.

---
+++ news/news/spiders/getNews.py
@@ -0,0 +1,55 @@
+import scrapy
+import time
+import requests
+import re
+from scrapy_selenium import SeleniumRequest
+from selenium import webdriver
+from requests.models import Request
+
+coinURLs = []
+
+class NewsSpider(scrapy.Spider):
+  name = "news"
+  allowed_urls = ["https://coinmarketcap.com/"]
+
+  # start_urls = coinURLs
+  start_urls = [
+    'https://coinmarketcap.com/currencies/bitcoin/news', 
+    'https://coinmarketcap.com/currencies/ethereum/news', 
+    'https://coinmarketcap.com/currencies/calypso/news/'
+  ]
+
+  def parse(self, response):
+    numberOfNews = 3
+    selectors = response.css('.svowul-5 .svowul-0')[:numberOfNews]
+    coinIdSearch = re.search('https://coinmarketcap.com/currencies/(.*?)/news', response.url)
+    coinId = coinIdSearch.group(1)
+
+    if response.css('button.active span::text').get() == 'Latest News':
+      yield {
+        'id': coinId,
+        'headers': [re.sub(u"(\u2018|\u2019)", "'", header) for header in selectors.css('.svowul-2 h3::text').getall()],
+        'descriptions': [re.sub(u"(\u2018|\u2019)", "'", description) for description in selectors.css('.svowul-2 p::text').re(r'(^.*\.(?=\s)|^.*\.$)')],
+        'urls': [ "https://coinmarketcap.com" + url if url[0] == "/" else url for url in selectors.css('::attr(href)').getall()]
+      }
+      
+    # with open("response.html", "wb") as f:
+    #   f.write(response.css('h3::text').getall())
+
+def has_numbers(inputString):
+  return any(char.isdigit() for char in inputString)
+
+def getCoinNewsURL():
+  r = requests.get("https://api.coingecko.com/api/v3/coins/list")
+  if r.status_code != 200:
+      print("Error while getting coin ids.")
+      return
+  data = r.json()
+
+  for obj in data:
+    coinId = obj["id"]
+    if not has_numbers(coinId):
+      coinURLs.append(f"https://coinmarketcap.com/currencies/{coinId}/news")
+
+getCoinNewsURL()
+# print(coinURLs[:10])
