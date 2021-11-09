11b020455cb514968d540e9d8d38545c80356297
2dukes <ruipinto02@hotmail.com>
2021-11-09 09:45:16 +0000

    Cleaned values with empty arrays.

--- scripts/clean.py
+++ scripts/clean.py
@@ -7,7 +7,13 @@ coins = coins.drop(["symbol", "name"], axis=1)
 
 # Clean unicode characters in news headers and descriptions 
 coins.headers = coins.headers.apply(lambda x: str(x).replace('\\xa0',' ').replace('\\u200b', '') if str(x) != "nan" else "")
-coins.descriptions = coins.descriptions.apply(lambda x: str(x).replace('\\xa0',' ').replace('\\u200b', '') if str(x) != "nan" else "")
+coins.descriptions = coins.descriptions.apply(lambda x: str(x).replace('\\xa0',' ').replace('\\u200b', '') if str(x) != "nan" and str(x) != "[]" else "")
+
+# Clean columns with [] values
+coins.github = coins.github.apply(lambda x: "" if str(x) == "[]" else str(x))
+coins.categories = coins.categories.apply(lambda x: "" if str(x) == "[]" else str(x))
+coins.blockchain_site = coins.blockchain_site.apply(lambda x: "" if str(x) == "[]" else str(x))
+coins.homepage_link = coins.homepage_link.apply(lambda x: "" if str(x) == "[]" else str(x))
 
 # Save to file
 coins.to_csv("files/clean_coins.csv")
