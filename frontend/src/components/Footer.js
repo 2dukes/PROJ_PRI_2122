ec86bf2cdef8714364310024505325c1527ad60c
2dukes <ruipinto02@hotmail.com>
2021-12-22 23:10:08 +0000

    Add: MUI Header and Footer.

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,10 +1,19 @@
 import React from "react"
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
 const Footer = () => {
+    let windowHeight = window.innerHeight;
+
     return (
-        <div>
-            Footer
-        </div>
+        <AppBar position="absolute" color="primary" sx={{ "top": windowHeight - 64 + "px"}}>
+            <Toolbar>
+            <Typography width="100%" textAlign="right">
+                © 2021 PRI
+            </Typography>
+            </Toolbar>
+        </AppBar>
     );
 };
 
--- frontend/src/components/Layout/PageLayout.js
+++ frontend/src/components/Layout/PageLayout.js
@@ -1,14 +1,14 @@
 import React from "react";
-
+import { Fragment } from "react";
 import Header from "../Header";
 import Footer from "../Footer";
 
 const PageLayout = ({ children }) => (
-    <div>
+    <Fragment>
         <Header />
-            {children}
+        {children}
         <Footer />
-    </div>
+    </Fragment>
 );
 
 export default PageLayout;
