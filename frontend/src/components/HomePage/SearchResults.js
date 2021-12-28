a081ffebd6691fa15b5b73d1e1b8391ca3461da6
Tiago Gomes <up201806658@fe.up.pt>
2021-12-28 00:15:10 +0000

    Add: pagination

--- frontend/src/components/HomePage/SearchResults.js
+++ frontend/src/components/HomePage/SearchResults.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { Stack, Pagination } from "@mui/material";
 
 import SearchResultsItem from "./SearchResultsItem";
 
 const SearchResults = ({ searchResultsCryptos }) => {
+    const [currentPage, setPage] = useState(1);
+    const resultsPerPage = 3;
+    const numberPages = Math.ceil(searchResultsCryptos.length / resultsPerPage);
+    const indexOfLastResult = currentPage * resultsPerPage;
+    const indexOfFirstResult = indexOfLastResult - resultsPerPage;
+    const selectedResults = searchResultsCryptos.slice(indexOfFirstResult, indexOfLastResult);
+
     return (
         <div>
-            {searchResultsCryptos.map((searchResultsCrypto) => (
-                <SearchResultsItem searchResultsCrypto={searchResultsCrypto} />
-            ))}
+            <div>
+                {selectedResults.map((searchResultsCrypto) => (
+                    <SearchResultsItem searchResultsCrypto={searchResultsCrypto} />
+                ))}
+            </div>
+            <Stack spacing={2} alignItems="center" marginTop="2em">
+                <Pagination
+                    count={numberPages}
+                    page={currentPage}
+                    onChange={(_, newPage) => setPage(newPage)}
+                />
+            </Stack>
         </div>
     );
 };
