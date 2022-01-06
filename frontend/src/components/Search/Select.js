4e6317cb67b03e34803393be0cb3c4d16a281b0d
2dukes <ruipinto02@hotmail.com>
2022-01-06 10:38:36 +0000

    Add: Sort By Select in form.

---
+++ frontend/src/components/Search/Select.js
@@ -0,0 +1,33 @@
+import { memo } from "react";
+import Box from "@mui/material/Box";
+import FormControl from "@mui/material/FormControl";
+import { styled } from "@mui/material/styles";
+import { Select, MenuItem } from "@mui/material";
+
+const CustomFormControl = styled(FormControl)({
+    marginBottom: "1em",
+    width: "65%",
+    "@media(max-width: 1536px)": {
+        width: "100%",
+    },
+});
+
+const SelectWithInputs = ({ value, setValue }) => {
+    return (
+        <Box>
+            <CustomFormControl>
+                <Select
+                    value={value}
+                    onChange={(event) => setValue(event.target.value)}
+                    sx={{ height: "35px" }}
+                >
+                    <MenuItem value={"sortByScoreDesc"}>Descending Score</MenuItem>
+                    <MenuItem value={"sortByScoreAsc"}>Ascending Score</MenuItem>
+                    <MenuItem value={"sortByID"}>Crypto ID</MenuItem>
+                </Select>
+            </CustomFormControl>
+        </Box>
+    );
+};
+
+export default memo(SelectWithInputs);
