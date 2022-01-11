caa226d8bd85f92fbc39499f684a69400ace11da
MechJM <matosjoaops@protonmail.com>
2022-01-11 15:16:30 +0000

    Updated config script

--- synonyms/addToConfig.py
+++ synonyms/addToConfig.py
@@ -4,11 +4,11 @@ import json
 synonyms_filename = sys.argv[1]
 synonyms = ""
 
-with open("synonyms/original_synonyms.txt", "r") as original:
-    synonyms = original.read().strip(",").split(",\n")
+# with open("synonyms/original_synonyms.txt", "r") as original:
+#     synonyms = original.read().strip(",").split(",\n")
 
 with open(synonyms_filename, "r") as synonyms_file:
-    synonyms.extend(synonyms_file.read().strip(",").split(",\n"))
+    synonyms = synonyms_file.read().strip(",").split(",\n")
 
 synonyms = list(map(lambda synonym: synonym.strip("\""), synonyms))
 
