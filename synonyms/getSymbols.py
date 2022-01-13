4d2281ad96acb6b2f7797bf2a117800c9e37c413
MechJM <matosjoaops@protonmail.com>
2022-01-13 18:55:40 +0000

    Configs original, 1 and 2 have enough results for all queries

--- synonyms/getSymbols.py
+++ synonyms/getSymbols.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
+stopwords = ["a", "an", "and", "are", "as", "at", "be", "but", "by",
+    "for", "if", "in", "into", "is", "it",
+    "no", "not", "of", "on", "or", "such",
+    "that", "the", "their", "then", "there", "these",
+    "they", "this", "to", "was", "will", "with"]
+
 filename = "files/coins.csv"
 new_synonyms_filename = "synonyms/symbol_synonyms.txt"
 
@@ -11,6 +17,14 @@ ids = data["id"]
 
 with open(new_synonyms_filename, "w") as file:
     for id, symbol in zip(ids, symbols):
+        skip = False
+        split_id = id.split("-")
+        for part in split_id:
+            if part.lower() in stopwords:
+                skip = True
+                break
+        if skip:
+            continue
         symbol = str(symbol).replace("$", "")
-        if len(symbol.split(" ")) == 1:
+        if len(symbol.split(" ")) == 1 and symbol.lower() not in stopwords and id.lower() not in stopwords:
             file.write(f"\"{id}, {symbol}\",\n")
