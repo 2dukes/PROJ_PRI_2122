b7343a8b443798c78995d4c3918a94bbdec26aa8
MechJM <matosjoaops@protonmail.com>
2022-01-13 11:53:53 +0000

    One config tested

--- elasticsearch/init.sh
+++ elasticsearch/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Specify mappings
-curl -XPUT "http://localhost:9200/cryptos" -H 'Content-Type: application/json' --data-binary '@config.json'
+curl -XPUT "http://localhost:9200/cryptos" -H 'Content-Type: application/json' --data-binary '@new_config.json'
 
 # Feed Elastic Search
-curl -X POST "http://localhost:9200/cryptos/_bulk" -H 'Content-Type: application/json' --data-binary '@final_json.json'
+curl -X POST "http://localhost:9200/cryptos/_bulk" -H 'Content-Type: application/json' --data-binary '@subset_dataset.json'
--- scripts/generateSubsetDataset.py
+++ scripts/generateSubsetDataset.py
@@ -2,33 +2,35 @@ import json
 import random
 
 jsonFilePath = "files/clean_coins_ndjson.json"
-subsetFilePath = "elasticsearch/subset_dataset.json"
+subsetFilePath = "elasticsearch/subset_dataset_new.json"
 
 random_ids = [
-    89, 856, 441, 960, 4033, 5672,
-    890, 8560, 4410, 9000, 33, 672, 
-    393, 2507, 732, 2399, 2936, 1240,
-    3263, 446, 4693, 4837, 4838, 4879,
-    4939, 4956, 4959, 5658, 6678, 3503,
-    995, # bitcoin
-    2990 # ethereum
+    # 89, 856, 441, 960, 4033, 5672,
+    # 890, 8560, 4410, 9000, 33, 672,
+    # 393, 2507, 732, 2399, 2936, 1240,
+    # 3263, 446, 4693, 4837, 4838, 4879,
+    # 4939, 4956, 4959, 5658, 6678, 3503,
+    # 995,  # bitcoin
+    # 2990  # ethereum
+    1341, 5537, 7790, 2238, 2239, 2927, 3935
 ]
 
-NUMBER_OF_ROWS = 100 - 6*5 - 2
-TOTAL_IDS = 9575
+# NUMBER_OF_ROWS = 100 - 6*6 - 2
+# TOTAL_IDS = 9575
 
-for _ in range(NUMBER_OF_ROWS):
-    random_id = random.randint(1, TOTAL_IDS)
-    while (random_id in random_ids):
-        random_id = random.randint(1, TOTAL_IDS)
-    
-    random_ids.append(random_id)
+# for _ in range(NUMBER_OF_ROWS):
+#     random_id = random.randint(1, TOTAL_IDS)
+#     while (random_id in random_ids):
+#         random_id = random.randint(1, TOTAL_IDS)
+
+#     random_ids.append(random_id)
+
 
 with open(jsonFilePath, "r") as file:
     for c, line in enumerate(file.readlines()):
         line_num = c + 1
         if line_num in random_ids:
             with open(subsetFilePath, "a+") as subset_file:
-                new_line = '{"index": {"_id":' + str(line_num) +'}}\n'
-                subset_file.write(new_line)                
+                new_line = '{"index": {"_id":' + str(line_num) + '}}\n'
+                subset_file.write(new_line)
                 subset_file.write(line)
